b40f8bd3095acaa73ace4fd6d14b6d3691c9dc27
BoQuan <mo791206@gmail.com>
2021-07-29 17:00:44 +0800

    新增 血量

--- Enemy.cs
+++ Enemy.cs
@@ -1,28 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public float startSpeed = 10f;
+
     [HideInInspector]
     public float speed;
 
-
-    public float health = 100;
+    public float startHealth = 100f;
+    private float health;
 
     public int worth = 50;
 
     public GameObject deathEffect;
 
+    [Header("Unity Stuff")]
+    public Image healthBar;
+
+
     private void Start()
     {
         speed = startSpeed;
+        health = startHealth;
     }
 
     public void TakeDamage(float amount)//會從別的腳本傳一個值當作amount來用。
     {
         health -= amount;
+
+        healthBar.fillAmount=health/startHealth;
 
         if(health <= 0)
         {
