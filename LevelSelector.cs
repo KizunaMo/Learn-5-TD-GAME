131c20ecae91254b6c86cab931e88de83122a8fc
BoQuan <mo791206@gmail.com>
2021-08-03 16:06:47 +0800

    新增Level Select

---
+++ LevelSelector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    public SceneFader fader;
+
+
+    public void Selecte(string levelname)
+    {
+        
+        fader.FadeTo(levelname);
+        
+
+    }
+
+}
